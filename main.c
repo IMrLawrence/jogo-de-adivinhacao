9e4d56abe6a21cfaea809bd06879d6b4135837ac
Mr. Lawrence <75909919+IMrLawrence@users.noreply.github.com>
2022-03-06 14:59:35 -0300

    Adiciona sistema de dificuldade

--- main.c
+++ main.c
@@ -9,8 +9,9 @@ int main()
 	int numero_secreto = rand() % 100; /* Pega o resto da divisão de 100 */
 	int chute = 0;
 
-	int numero_tentativas = 6;
 	int contador_tentativas = 1;
+	int numero_tentativas = 6;
+	int dificuldade = 0;
 
 	int acertou = 0;
 
@@ -20,6 +21,33 @@ int main()
 	printf("*************************************\n");
 
 	printf("\n");
+
+	/* Seleciona dificuldade */
+	printf("******* Dificuldades *******\n");
+	printf("(1) Fácil - 20 Tentativas\n");
+	printf("(2) Médio - 15 Tentativas\n");
+	printf("(3) Difícil - 6 Tentativas\n");
+	printf("Selecione a dificuldade desejada: ");
+	scanf("%d", &dificuldade);
+
+	if (dificuldade <= 0 || dificuldade >= 4) {
+		printf("Por favor, selecione uma dificuldade válida!\n");
+		return (1);
+	}
+
+	switch (dificuldade)
+	{
+		case 1:
+			numero_tentativas = 20;
+			break;
+		case 2:
+			numero_tentativas = 15;
+			break;
+		case 3:
+			numero_tentativas = 6;
+	}
+
+	printf("****************************\n\n");
 
 	/* Loop do Jogo */
 	for (int i = 1; i <= numero_tentativas; i++)
@@ -47,7 +75,7 @@ int main()
 	if (acertou)
 	{
 		printf("\nParabéns!\n");
-		printf("Você acertou em %d tentativas!\n", contador_tentativas);
+		printf("Você acertou em %d tentativas de %d!\n", contador_tentativas, numero_tentativas);
 	}
 	else
 	{
