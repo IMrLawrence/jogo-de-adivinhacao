407d3ef64e6ca2f11ef9e0a9ba07eecc83beef41
Mr. Lawrence <75909919+IMrLawrence@users.noreply.github.com>
2022-03-06 13:32:51 -0300

    Adiciona o loop do jogo e faz as implementações iniciais

--- main.c
+++ main.c
@@ -2,11 +2,42 @@
 
 int main()
 {
+	/* Variáveis */
+	int numero_secreto = 42;
+	int chute = 0;
+
+	int contador_tentativas = 1;
+
+	/* Cabeçalho do Jogo */
 	printf("*************************************\n");
 	printf("* Bem vindo ao Jogo de Adivinhação! *\n");
 	printf("*************************************\n");
 
-	printf("\n\n");
+	printf("\n");
+
+	/* Loop do Jogo */
+	while (1)
+	{
+		printf("Tentativa %d\n", contador_tentativas);
+
+		printf("Digite seu chute: ");
+		scanf("%d", &chute);
+
+		/* Se tiver acertado*/
+		if (chute == numero_secreto)
+			break;
+		
+		/* Se tiver errado */
+		else if (chute > numero_secreto)
+			printf("Seu chute foi MAIOR do que o número gerado!\n\n");
+		else if (chute < numero_secreto)
+			printf("Seu chute foi MENOR do que o número gerado!\n\n");
+
+		contador_tentativas++;
+	}
+
+	printf("\nParabéns!\n");
+	printf("Você acertou em %d tentativas!\n", contador_tentativas);
 
 	return (0);
 }
